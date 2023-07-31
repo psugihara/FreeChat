ec6935deedbb57db303dd25c2474b2e422228dea
psugihara <peter@campsh.com>
2023-07-31 10:45:12 -0700

    Initial Commit

---
+++ Mantras/MantrasApp.swift
@@ -0,0 +1,20 @@
+//
+//  MantrasApp.swift
+//  Mantras
+//
+//  Created by Peter Sugihara on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MantrasApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ MantrasUITests/MantrasUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MantrasUITestsLaunchTests.swift
+//  MantrasUITests
+//
+//  Created by Peter Sugihara on 7/31/23.
+//
+
+import XCTest
+
+final class MantrasUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
