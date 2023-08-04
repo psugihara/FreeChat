9d521134b27566fb9839971ae7dbb5b39cae0a19
psugihara <peter@campsh.com>
2023-08-04 08:35:57 -0700

    File > New Conversation

--- Chats/ChatsApp.swift
+++ Chats/ChatsApp.swift
@@ -9,15 +9,20 @@ import SwiftUI
 
 @main
 struct ChatsApp: App {
-    let persistenceController = PersistenceController.shared
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
-        .commands {
-          SidebarCommands()
-        }
+  let persistenceController = PersistenceController.shared
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .environment(\.managedObjectContext, persistenceController.container.viewContext)
     }
+    .commands {
+      CommandGroup(replacing: .newItem) {
+        Button("New Chat") {
+          _ = try! Conversation.create(ctx: persistenceController.container.viewContext)
+        }.keyboardShortcut(KeyboardShortcut("N"))
+      }
+      SidebarCommands()
+    }
+  }
 }
