37fe6f89b7a7d9efa30dbe47d7ec75244a5f3b0f
psugihara <peter@campsh.com>
2023-10-19 16:18:46 -0700

    lol

--- web/app/TiltyApp.tsx
+++ web/app/TiltyApp.tsx
@@ -5,14 +5,22 @@ import Image from "next/image";
 
 export default function TiltyApp() {
   return (
-    <Tilt glareEnable={true} glareMaxOpacity={0.45}>
+    <Tilt
+      className="group"
+      tiltMaxAngleX={40}
+      tiltMaxAngleY={20}
+      perspective={2000}
+      transitionSpeed={1500}
+      scale={1.05}
+      tiltReverse={true}
+    >
       <Image
         src="/tidles-1024.png"
         alt="FreeChat app icon"
         width={256}
         height={256}
         priority
-        className="transition-translate duration-300 group-hover:-translate-y-2 group-hover:translate-x-2 group-active:translate-x-0 group-active:translate-y-0 group-active:skew-y-0 transform-gpu skew-y-0 hover:skew-y-2 transition-transform motion-reduce:transform-none"
+        className="transition-translate duration-300 translate-z-96 group-hover:-translate-y-2 group-hover:translate-x-2 group-active:translate-x-0 group-active:translate-y-0 transform-gpu transition-transform motion-reduce:transform-none"
       />
       <Image
         src="/tidles-1024.png"
@@ -20,7 +28,7 @@ export default function TiltyApp() {
         width={256}
         height={256}
         priority
-        className="absolute top-0 left-0 opacity-0 group-active:opacity-0 transition-opacity group-hover:opacity-50 invert dark:invert-0 z-[-1] duration-500"
+        className="absolute top-0 left-0 opacity-0 -translate-z-96 group-active:opacity-0 transition-opacity group-hover:opacity-50 invert dark:invert-0 z-[-1] duration-500"
       />
     </Tilt>
   );
