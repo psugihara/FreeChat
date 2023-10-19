cdfb38a349fd1857260fe0311c1ff508c11bf923
psugihara <peter@campsh.com>
2023-10-19 09:35:22 -0700

    footer

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,22 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'FreeChat',
-  description: 'Local, secure, open-source AI chat for macOS',
-}
+  title: "FreeChat",
+  description: "Local, secure, open-source AI chat for macOS",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <footer className="flex flex-col items-center text-center text-white dark:bg-black text-neutral-800 dark:text-neutral-400">
+          <div className="w-full p-4 text-center">
+            © 2023{" "}
+            <a
+              href="https://x.com/_0_"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Peter Sugihara
+            </a>
+          </div>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,24 +3,37 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[500px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-gray-200 after:via-violet-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-violet-700 before:dark:opacity-10 after:dark:from-white after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-1">
-        <h1 className="text-6xl font-black">FreeChat</h1>
-        <h2 className="text-2xl block pt-3">
+      <div className="flex flex-col place-items-center relative">
+        <h1 className="text-6xl font-black before:absolute before:h-[300px] before:w-[500px] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[340px] after:-translate-x-1/3 after:bg-gradient-conic after:from-gray-200 after:via-violet-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-violet-700 before:dark:opacity-10 after:dark:from-white after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-10">
+          FreeChat
+        </h1>
+        <h2 className="text-2xl block pt-3 z-50">
           Local, secure, open-source AI chat for macOS
         </h2>
         <a
-          className="text-center group transform-gpu skew-y-0 hover:skew-y-2 transition-transform duration-300 ease-in-out motion-reduce:transform-none"
+          className="z-50 text-center group"
           href="https://6032904148827.gumroad.com/l/freechat-beta?_gl=1*1qow0km*_ga*MjEwOTUwNzk3MC4xNjk1MjMyNjEz*_ga_6LJN6D94N6*MTY5NzY5MzA5MS4xNC4xLjE2OTc2OTMwOTQuMC4wLjA."
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            src="/tidles-1024.png"
-            alt="FreeChat app icon"
-            width={256}
-            height={256}
-            priority
-          />
+          <div className="relative">
+            <Image
+              src="/tidles-1024.png"
+              alt="FreeChat app icon"
+              width={256}
+              height={256}
+              priority
+              className="transition-translate duration-300 group-hover:-translate-y-2 group-hover:translate-x-2 group-active:translate-x-0 group-active:translate-y-0 group-active:skew-y-0 transform-gpu skew-y-0 hover:skew-y-2 transition-transform duration-300 ease-in-out motion-reduce:transform-none"
+            />
+            <Image
+              src="/tidles-1024.png"
+              alt="FreeChat app icon"
+              width={256}
+              height={256}
+              priority
+              className="absolute top-0 left-0 opacity-0 group-active:opacity-0 transition-opacity group-hover:opacity-100 invert z-[-1] duration-500"
+            />
+          </div>
 
           <h2 className="mb-3 text-2xl font-semibold">
             Download it{" "}
