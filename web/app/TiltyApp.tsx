1b9b20d4700930269ac4c20f7a887a46183fda5a
psugihara <peter@campsh.com>
2023-10-19 20:39:38 -0700

    lighter

--- web/app/TiltyApp.tsx
+++ web/app/TiltyApp.tsx
@@ -5,23 +5,25 @@ import Image from "next/image";
 
 export default function TiltyApp() {
   return (
-    <Tilt
-      className="group"
-      tiltMaxAngleX={40}
-      tiltMaxAngleY={20}
-      perspective={2000}
-      transitionSpeed={1500}
-      scale={1.05}
-      tiltReverse={true}
-    >
-      <Image
-        src="/tidles-1024.png"
-        alt="FreeChat app icon"
-        width={256}
-        height={256}
-        priority
-        className="transition-translate duration-300 translate-z-96 group-hover:-translate-y-2 group-hover:translate-x-2 group-active:translate-x-0 group-active:translate-y-0 transform-gpu transition-transform motion-reduce:transform-none"
-      />
+    <>
+      <Tilt
+        className="group"
+        tiltMaxAngleX={40}
+        tiltMaxAngleY={20}
+        perspective={2000}
+        transitionSpeed={1500}
+        scale={1.05}
+        tiltReverse={true}
+      >
+        <Image
+          src="/tidles-1024.png"
+          alt="FreeChat app icon"
+          width={256}
+          height={256}
+          priority
+          className="transition-translate duration-300 translate-z-96 group-hover:-translate-y-2 group-hover:translate-x-2 group-active:translate-x-0 group-active:translate-y-0 transform-gpu transition-transform motion-reduce:transform-none"
+        />
+      </Tilt>
       <Image
         src="/tidles-1024.png"
         alt="FreeChat app icon"
@@ -30,6 +32,6 @@ export default function TiltyApp() {
         priority
         className="absolute top-0 left-0 opacity-0 -translate-z-96 group-active:opacity-0 transition-opacity group-hover:opacity-50 invert dark:invert-0 z-[-1] duration-500"
       />
-    </Tilt>
+    </>
   );
 }
