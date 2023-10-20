37103ab8b3d5fb89b7a20dc83a12d51bb795602f
psugihara <peter@campsh.com>
2023-10-20 11:52:10 -0700

    cursor

--- web/app/TiltyApp.tsx
+++ web/app/TiltyApp.tsx
@@ -2,14 +2,23 @@
 
 import Tilt from "react-parallax-tilt";
 import Image from "next/image";
+import { useEffect } from "react";
+import { emojiCursor } from "cursor-effects";
+
+function activateCursor() {
+  emojiCursor({ emoji: ["🔥", "🐬", "🦆"] });
+}
 
 export default function TiltyApp() {
   return (
-    <div className="rounded-3xl transition-all group-hover:bg-gray-200">
+    <div
+      className="rounded-3xl transition-all group-hover:bg-gray-200 dark:group-hover:bg-gray-600"
+      onMouseEnter={activateCursor}
+    >
       <Tilt
         className="group"
-        tiltMaxAngleX={15}
-        tiltMaxAngleY={15}
+        tiltMaxAngleX={20}
+        tiltMaxAngleY={20}
         perspective={2000}
         transitionSpeed={1500}
         scale={1.04}
@@ -18,10 +27,10 @@ export default function TiltyApp() {
         <Image
           src="/tidles-1024.png"
           alt="FreeChat app icon"
-          width={200}
-          height={200}
+          width={128}
+          height={128}
           priority
-          className="bg-white transition-all duration-300 translate-z-96 group-hover:-translate-y-3 group-hover:translate-x-3 group-active:translate-x-0 group-active:translate-y-0 transform-gpu motion-reduce:transform-none"
+          className="bg-white shadow-icon rounded-3xl transition-all duration-300 translate-z-96 group-hover:-translate-y-3 group-hover:translate-x-3 group-hover:skew-y-6 group-active:translate-x-0 group-active:translate-y-0 transform-gpu motion-reduce:transform-none"
         />
       </Tilt>
     </div>
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,32 +1,38 @@
+import Image from "next/image";
 import TiltyApp from "./TiltyApp";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="flex flex-col  relative">
-        <div className="relative -ml-10">
-          <TiltyApp />
-        </div>
-        <h1 className="text-9xl font-semibold">FreeChat</h1>
-        <h2 className="text-5xl block pt-4 pb-12 z-50 max-w-[660px] leading-tight">
+        <Image
+          src="/tidles-1024.png"
+          alt="FreeChat app icon"
+          width={64}
+          height={64}
+          priority
+          className="mb-16 bg-white sm:hidden shadow-icon rounded-3xl transition-all duration-300 translate-z-96 group-hover:-translate-y-3 group-hover:translate-x-3 group-hover:skew-y-6 group-active:translate-x-0 group-active:translate-y-0 transform-gpu motion-reduce:transform-none"
+        />
+        <h1 className="text-6xl sm:text-9xl font-semibold">FreeChat</h1>
+        <h2 className="text-2xl sm:text-5xl block pt-4 pb-6 sm:pb-12 max-w-[330px] sm:max-w-[660px] leading-tight sm:leading-tight">
           Local, secure, open source AI chat for macOS
         </h2>
-        {/* <a
-          className="z-50 group duration-300 ease-in-out flex items-center justify-between"
+        <a
+          className="group duration-300 ease-in-out flex items-center justify-between"
           href="https://6032904148827.gumroad.com/l/freechat-beta?_gl=1*1qow0km*_ga*MjEwOTUwNzk3MC4xNjk1MjMyNjEz*_ga_6LJN6D94N6*MTY5NzY5MzA5MS4xNC4xLjE2OTc2OTMwOTQuMC4wLjA."
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-6xl font-semibold text-slate-800 hover:text-black">
-            Download it{" "}
+          <h2 className="mb-3 text-3xl sm:text-6xl font-semibold light:text-slate-800 light:hover:text-black">
+            Try the beta{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <div className="relative pb-4 pr-12">
+          <div className="pb-4 pr-12 hidden sm:block">
             <TiltyApp />
           </div>
-        </a> */}
+        </a>
       </div>
     </main>
   );
