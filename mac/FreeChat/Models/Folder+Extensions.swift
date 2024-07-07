a005aa7977e9d20d56ce5c998205f16c8ff0f73b
Evolving Software <development@evolvingsoftware.com>
2024-07-07 11:31:30 +1000

    Add Folder Datatype

---
+++ mac/FreeChat/Models/Folder+Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  Folder+Extensions.swift
+//  FreeChat
+//
+//  Created by Sebastian Gray on 5/7/2024.
+//
+
+import Foundation
+import CoreData
+
+extension Folder {
+    
+  static func create(ctx: NSManagedObjectContext, name: String, parent: Folder? = nil) throws -> Self {
+    let folder = self.init(context: ctx)
+    folder.name = name
+    folder.parent = parent
+    try ctx.save()
+    return folder
+  }
+    
+  var subfolders: [Folder] {
+      let childFolders = self.child as? Set<Folder> ?? []
+        
+      return Array(childFolders).sorted { $0.name ?? "" < $1.name ?? "" }
+  }
+  
+  func addSubfolder(_ subfolder: Folder) {
+      addToChild(subfolder)
+  }
+  
+  func setSysPrompt(_ prompt: String?) {
+      self.sysPrompt = prompt
+  }
+  
+  public override func willSave() {
+      super.willSave()
+      
+      if !isDeleted, changedValues()["updatedAt"] == nil {
+          self.setValue(Date(), forKey: "updatedAt")
+      }
+  }
+}
